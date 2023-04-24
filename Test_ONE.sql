475d168956ed9f0b06bfe2e18c309d15e13f6c96
Sami Munir <smunir2001@gmail.com>
2023-04-24 18:20:25 -0400

    building new database-University using Test_ONE script

---
+++ Test_ONE.sql
@@ -0,0 +1,24 @@
+-- show databases;
+-- -- create database University;
+-- -- show databases;
+-- use university;
+-- create table EngineeringStudents(
+-- 	Student_ID INT NOT NULL,
+--     Department VARCHAR(25),
+--     First_Name VARCHAR(25),
+--     Last_Name VARCHAR(25),
+--     PassOutYear INT NOT NULL,
+--     UniversityRank INT NOT NULL,
+--     PRIMARY KEY ( STUDENT_ID )
+-- );
+
+-- show databases;
+-- use university;
+-- show tables;
+
+-- INSERT INTO EngineeringStudents VALUES (10201, 'CSE', 'Kiran', 'Acharya', 2018, 1272);
+-- INSERT INTO EngineeringStudents VALUES (10202, 'ISE', 'Aditya', 'Chuhan', 2019, 1249);
+-- INSERT INTO EngineeringStudents VALUES (10203, 'ECE', 'John', 'Doe', 2020, 1563);
+-- INSERT INTO EngineeringStudents VALUES (10204, 'CSE', 'Mary', 'Smith', 2019, 1423);
+
+SELECT * FROM EngineeringStudents;
