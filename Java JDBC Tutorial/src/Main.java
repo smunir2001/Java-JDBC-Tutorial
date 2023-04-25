810c3c4f169600b194b87cc3f33bab507b3bb70f
Sami Munir <smunir2001@gmail.com>
2023-04-24 21:20:37 -0400

    adding function runConnection()

--- Java JDBC Tutorial/src/Main.java
+++ Java JDBC Tutorial/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world\n\twith JDBC!");
+
+        Test_JDBC obj = new Test_JDBC();
     }
 }
--- Java JDBC Tutorial/src/Test_JDBC.java
+++ Java JDBC Tutorial/src/Test_JDBC.java
@@ -1,2 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Test_JDBC {
+
+    public Test_JDBC() {}
+
+    public void runConnection() throws SQLException {
+        //
+    }
 }
